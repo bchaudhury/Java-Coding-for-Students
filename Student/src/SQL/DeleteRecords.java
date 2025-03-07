d682c90ec48c6c6199921d658d84bb7468c5b98e
BHASKAR CHAUDHURY <chaudhury.bhaskar@gmail.com>
2025-03-07 16:51:08 +0530

    7/3/2025 Commit Rev-2

---
+++ Student/src/SQL/DeleteRecords.java
@@ -0,0 +1,49 @@
+// Update records in the table 'Registration' in the database 'STUDENTS'
+// Importing required libraries
+
+package SQL;
+
+import java.sql.*;
+
+public class DeleteRecords {
+
+    // JDBC driver name, database URL, User and Password
+    static final String DB_URL = "jdbc:mysql://localhost/STUDENTS";
+    static final String USER = "root";
+    static final String PASS = "Bhas@123abc#";
+    static final String QUERY = "SELECT id, first, last, age FROM Registration";
+ 
+    // Main method
+    public static void main(String[] args) {
+       // Open a connection
+       try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+          Statement stmt = conn.createStatement();
+       ) {	
+          // Execute a query	
+          System.out.println("Deleting records in the table...");      
+          String sql = "DELETE from Registration " +
+             "WHERE id = 100";
+          stmt.executeUpdate(sql);
+          ResultSet rs = stmt.executeQuery(QUERY);
+
+          // Extract data from result set
+          while(rs.next()){
+             //Display values
+             System.out.print("ID: " + rs.getInt("id"));
+             System.out.print(", Age: " + rs.getInt("age"));
+             System.out.print(", First: " + rs.getString("first"));
+             System.out.println(", Last: " + rs.getString("last"));
+          }
+
+          // Close resources
+          rs.close();
+          conn.close();
+          stmt.close();
+
+          } catch (SQLException e) {
+             e.printStackTrace();
+          } 
+
+    }
+
+}
