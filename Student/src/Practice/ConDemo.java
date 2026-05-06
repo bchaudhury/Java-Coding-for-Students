024811ce57d39470cd71911cc3716754149b9924
BHASKAR CHAUDHURY <chaudhury.bhaskar@gmail.com>
2026-05-06 20:39:53 +0530

    New

---
+++ Student/src/Practice/ConDemo.java
@@ -0,0 +1,15 @@
+package Practice;
+
+public class ConDemo {
+
+    public static void main(String[] args) {
+        
+        String str = "Hello World";
+        
+        char ch = str.charAt(0);
+        
+        System.out.println(ch);
+        
+    }
+
+}
